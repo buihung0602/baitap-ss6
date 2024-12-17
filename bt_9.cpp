1c8803e628a36e46c57b118d000fb2e1b61fe0ac
buihung0602 <n24dtcn035@student.ptithcm.edu.vn>
2024-12-17 15:19:28 +0700

    Add files via upload

--- bt_9.cpp
+++ bt_9.cpp
@@ -1,15 +1,14 @@
 #include <stdio.h>
-int a,b,c,product=1;
-int main() {
-	printf("cac so Armstrong co 3 chu so la: ");
-for (int i=100;i<1000;i++) {
-	a= i/100;
-	b= (i/10)%10;
-	c= i%10;
-	product=(a*a*a)+(b*b*b)+(c*c*c);
-	if (product==i) {
-		printf("%d  ",i);
+int a,b,c;
+int main () {
+	for(a=1;a<=9;a++) {
+		for(b=0;b<=9;b++) {
+			for(c=0;c<=9;c++){
+				if (a*a*a+b*b*b+c*c*c==a*100+b*10+c) {
+					printf("%d  ",a*100+b*10+c);
+				}
+			}
+		}
 	}
-} 
 	return 0;
-}
+} 
